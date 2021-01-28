b0c6910274ec2fcda7cc37bb14f8b28482208e47
Franco De Alesandro <fdealesandro@tgv.com.ar>
2021-01-28 15:10:11 -0300

    Se agregan parametros onload y onerror

--- authGoogle.js
+++ authGoogle.js
@@ -1,65 +1,29 @@
-
-  export function authenticate() {
+  export function login(onLogin, onError) {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/user.phonenumbers.read"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  export function loadClient(apiKey) {
-    gapi.client.setApiKey(apiKey);
-    return gapi.client.load("https://people.googleapis.com/$discovery/rest?version=v1")
-        .then(function() { 
-            console.log("GAPI client loaded for APIs"); 
-            gapi.client.load('oauth2', 'v2', function () {
+        .then(gapi.client.load("https://people.googleapis.com/$discovery/rest?version=v1"))
+        .then(() => { 
+            gapi.client.load('oauth2', 'v2', () => {
                 var request = gapi.client.oauth2.userinfo.get({
-                    'userId': 'me'
+                    userId: 'me'
                 });
-                console.log(request);
-                request.execute(function (resp) {
-                    execute(resp.id); 
-                });
+                request.execute((resp) =>
+                  gapi.client.people.people.get({
+                    resourceName: `people/${resp.id}`,
+                    personFields: "birthdays,names,photos",
+                    sources: [
+                      "READ_SOURCE_TYPE_PROFILE"
+                    ]
+                  }).then((resp) => onLogin ? onLogin(resp) : null)
+                );
             });
             },
-        function(err) { console.error("Error loading GAPI client for API", err); });
+        (err) => onError ? onError(err) : null);
   }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute(UserId) {
-    return gapi.client.people.people.get({
-      "resourceName": `people/${UserId}`,
-      "personFields": "birthdays,names,photos",
-      "sources": [
-        "READ_SOURCE_TYPE_PROFILE"
-      ]
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-                console.log(response.result);
-                console.log('Redireccionando a: formDataUser.html');
-        
-                Swal.fire({
-                    title: 'Bienvenido!',
-                    text: response.result.names[0].displayName,
-                    icon: 'success',
-                    confirmButtonText: 'OK'
-                  }).then(function(){
-                      window.location = "http://localhost:8080/formDataUser.html";
-                  });
-
-              },
-              function(err) { 
-                console.error("Execute error", err);
-                Swal.fire({
-                  title: 'Error',
-                  text: 'No autorizado',
-                  icon: 'error',
-                  confirmButtonText: 'OK'
-                });
-             });
-  }
-
-  export default function initializeGoogle(client_id) {
-    gapi.load("client:auth2", function() {
-      gapi.auth2.init({client_id});
+  
+  export default function initializeGoogle(client_id, apiKey) {
+    gapi.load("client:auth2", () => {
+      gapi.auth2.init({client_id})
+      .then(() => gapi.client.setApiKey(apiKey));
     });
   }
--- script.js
+++ script.js
@@ -6,12 +6,42 @@
 // };
 // var mask = IMask(element, maskOptions);
 
-import initializeFacebook, { login } from './authFacebook.js';
-import initializeGoogle, { authenticate, loadClient } from './authGoogle.js';
+import initializeFacebook, { login as loginFB } from './authFacebook.js';
+import initializeGoogle, { login as LoginG } from './authGoogle.js';
 
 initializeFacebook('2605083403115042');
-document.getElementById('fAuth').onclick = login;
+document.getElementById('fAuth').onclick = () => loginFB(
+    (response) => {
+        Swal.fire({
+            title: 'Bienvenido!',
+            text: response.name,
+            icon: 'success',
+            confirmButtonText: 'OK'
+          })
+          .then(() => window.location = "http://localhost:8080/formDataUser.html");
+    },
+    (err) =>  Swal.fire({
+        title: 'Error',
+        text: 'No autorizado',
+        icon: 'error',
+        confirmButtonText: 'OK'
+    })
+);
 
-initializeGoogle("791551942698-8g31qv76mpfkm3ifce2qu0jb20ghr834.apps.googleusercontent.com");
-document.getElementById('gAuth').onclick = () => authenticate()
-    .then(loadClient("AIzaSyCI7dtel6JRXCWKBdXNEuxkStEr8EYaO6k"))
+initializeGoogle("791551942698-8g31qv76mpfkm3ifce2qu0jb20ghr834.apps.googleusercontent.com", "AIzaSyCI7dtel6JRXCWKBdXNEuxkStEr8EYaO6k");
+document.getElementById('gAuth').onclick = () => LoginG(
+    (response) => {
+        Swal.fire({
+            title: 'Bienvenido!',
+            text: response.result.names[0].displayName,
+            icon: 'success',
+            confirmButtonText: 'OK'
+          })
+          .then(() => window.location = "http://localhost:8080/formDataUser.html");
+    },
+    (err) =>  Swal.fire({
+        title: 'Error',
+        text: 'No autorizado',
+        icon: 'error',
+        confirmButtonText: 'OK'
+    }));
