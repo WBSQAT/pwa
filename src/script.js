b32954ba801b6850756b642cb636ca17e6326b35
Hernandez Illa <nhernandez@tgv.com.ar>
2021-01-28 16:07:41 -0300

    cambios webpack hot-reload

--- src/script.js
+++ src/script.js
@@ -7,106 +7,115 @@
 // var mask = IMask(element, maskOptions);
 
 
-function login() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
-          console.log('Successful login for: ' + response.name);
-          console.log(response);
-          console.log('Redireccionando a: formDataUser.html');
-          Swal.fire({
-            title: 'Bienvenido!',
-            text: response.name,
-            icon: 'success',
-            confirmButtonText: 'OK'
-          }).then(function(){
-              window.location = "http://localhost:8080/formDataUser.html";
-          });
-        });
-        } else {
-        console.log('User cancelled login or did not fully authorize.');
+export function login() {
+  FB.login(function(response) {
+      if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        console.log(response);
+        console.log('Redireccionando a: formDataUser.html');
         Swal.fire({
-          title: 'Error',
-          text: 'No autorizado',
-          icon: 'error',
+          title: 'Bienvenido!',
+          text: response.name,
+          icon: 'success',
           confirmButtonText: 'OK'
+        }).then(function(){
+            window.location = "http://localhost:8080/formDataUser.html";
         });
-        }
-    });
-  }
+      });
+      } else {
+      console.log('User cancelled login or did not fully authorize.');
+      Swal.fire({
+        title: 'Error',
+        text: 'No autorizado',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+      }
+  });
+}
 
 
 window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '2605083403115042',
-    cookie     : true,                     // Enable cookies to allow the server to access the session.
-    xfbml      : true,                     // Parse social plugins on this webpage.
-    version    : 'v9.0'           // Use this Graph API version for this call.
-  });
+FB.init({
+  appId      : '2605083403115042',
+  cookie     : true,                     // Enable cookies to allow the server to access the session.
+  xfbml      : true,                     // Parse social plugins on this webpage.
+  version    : 'v9.0'           // Use this Graph API version for this call.
+});
 
 };
 
 
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/user.phonenumbers.read"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyCI7dtel6JRXCWKBdXNEuxkStEr8EYaO6k");
-    return gapi.client.load("https://people.googleapis.com/$discovery/rest?version=v1")
-        .then(function() { 
-            console.log("GAPI client loaded for APIs"); 
-            gapi.client.load('oauth2', 'v2', function () {
-                var request = gapi.client.oauth2.userinfo.get({
-                    'userId': 'me'
-                });
-                console.log(request);
-                request.execute(function (resp) {
-                    execute(resp.id); 
-                });
-            });
-            },
-        function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute(UserId) {
-    return gapi.client.people.people.get({
-      "resourceName": `people/${UserId}`,
-      "personFields": "birthdays,names,photos",
-      "sources": [
-        "READ_SOURCE_TYPE_PROFILE"
-      ]
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-                console.log(response.result);
-                console.log('Redireccionando a: formDataUser.html');
-        
-                Swal.fire({
-                    title: 'Bienvenido!',
-                    text: response.result.names[0].displayName,
-                    icon: 'success',
-                    confirmButtonText: 'OK'
-                  }).then(function(){
-                      window.location = "http://localhost:8080/formDataUser.html";
-                  });
-
-              },
-              function(err) { 
-                console.error("Execute error", err);
-                Swal.fire({
-                  title: 'Error',
-                  text: 'No autorizado',
-                  icon: 'error',
+export function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: "https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/user.phonenumbers.read"})
+      .then(function() { console.log("Sign-in successful"); },
+            function(err) { console.error("Error signing in", err); });
+}
+export function loadClient() {
+  gapi.client.setApiKey("AIzaSyCI7dtel6JRXCWKBdXNEuxkStEr8EYaO6k");
+  return gapi.client.load("https://people.googleapis.com/$discovery/rest?version=v1")
+      .then(function() { 
+          console.log("GAPI client loaded for APIs"); 
+          gapi.client.load('oauth2', 'v2', function () {
+              var request = gapi.client.oauth2.userinfo.get({
+                  'userId': 'me'
+              });
+              console.log(request);
+              request.execute(function (resp) {
+                  execute(resp.id); 
+              });
+          });
+          },
+      function(err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+export function execute(UserId) {
+  return gapi.client.people.people.get({
+    "resourceName": `people/${UserId}`,
+    "personFields": "birthdays,names,photos",
+    "sources": [
+      "READ_SOURCE_TYPE_PROFILE"
+    ]
+  })
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Response", response);
+              console.log(response.result);
+              console.log('Redireccionando a: formDataUser.html');
+      
+              Swal.fire({
+                  title: 'Bienvenido!',
+                  text: response.result.names[0].displayName,
+                  icon: 'success',
                   confirmButtonText: 'OK'
+                }).then(function(){
+                    window.location = "http://localhost:8080/formDataUser.html";
                 });
-             });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "791551942698-8g31qv76mpfkm3ifce2qu0jb20ghr834.apps.googleusercontent.com"});
-  });
 
+            },
+            function(err) { 
+              console.error("Execute error", err);
+              Swal.fire({
+                title: 'Error',
+                text: 'No autorizado',
+                icon: 'error',
+                confirmButtonText: 'OK'
+              });
+           });
+}
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({client_id: "791551942698-8g31qv76mpfkm3ifce2qu0jb20ghr834.apps.googleusercontent.com"});
+});
+
+export function manageAuth() {
+  console.log("asszxczxczdd");
+  authenticate().then(loadClient);
+}
+
+$(document).ready(function () {
+  document.getElementById('gAuth').addEventListener('click', manageAuth);
+  document.getElementById('fAuth').addEventListener('click', login);
+});
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,15 +1,29 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/script.js',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        hot: true,
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+  mode: "development",
+  entry: "./src/script.js",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    watchContentBase: true, //if nothing to update looping message comment this!
+    hot: true,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      filename: "index.html",
+      template: "./public/index.html",
+    }),
+    // Provides jQuery for other JS bundled with Webpack
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+};
